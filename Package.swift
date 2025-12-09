90a0882e127a680b2db1d7656e0dc6e7fda438d4
twilio-sdk-build <twilio-sdk-build@twilio.com>
2025-12-09 17:46:17 +0000

    Update for 5.11.0

--- Package.swift
+++ Package.swift
@@ -11,12 +11,20 @@ let package = Package(
         .library(
             name: "TwilioVirtualBackgroundProcessors",
             targets: ["TwilioVirtualBackgroundProcessors"]),
+        .library(
+            name: "TwilioVirtualBackgroundProcessors-static",
+            targets: ["TwilioVirtualBackgroundProcessors"])
     ],
     targets: [
         .binaryTarget(
             name: "TwilioVirtualBackgroundProcessors",
-            url: "https://github.com/twilio/twilio-virtual-background-processors-ios/releases/download/5.10.0/TwilioVirtualBackgroundProcessors.xcframework.zip",
-            checksum: "44cca8e571bf4e64afaf2e7b1cb9f04c6fafc1dc5a90daccc501818bebd45f3b"
+            url: "https://github.com/twilio/twilio-virtual-background-processors-ios/releases/download/5.11.0/TwilioVirtualBackgroundProcessors.xcframework.zip",
+            checksum: "6a4dc18310c1db393d45446a92688c37f5b0518adf37d98987855ddb7133c4a9"
         ),
+        .binaryTarget(
+            name: "TwilioVirtualBackgroundProcessors-static",
+            url: "https://github.com/twilio/twilio-virtual-background-processors-ios/releases/download/5.11.0/TwilioVirtualBackgroundProcessors-static.xcframework.zip",
+            checksum: "30fa73cfef8b0ddcf119558cedb3a974376d3e06bd4d60cbb8f5ed2e603909be"
+        )
     ]
 )
