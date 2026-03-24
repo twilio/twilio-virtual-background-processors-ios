89e597b7d1b19e841e8dbe2f15409bdbb2880ced
Bobie Chen <bchen@twilio.com>
2026-03-24 16:51:35 -0700

    Revert "Update for 99.99.99"
    This reverts commit 4fb86c277340b8fb7797aaba904e27dd2cc5eab6.

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioVirtualBackgroundProcessors",
-            url: "https://github.com/twilio/twilio-virtual-background-processors-ios/releases/download/99.99.99/TwilioVirtualBackgroundProcessors.xcframework.zip",
-            checksum: "b17ff63da483118c7ff8e8efe8e28c559a1030fe1645041b5c4418d2298aa6e9"
+            url: "https://github.com/twilio/twilio-virtual-background-processors-ios/releases/download/5.11.2/TwilioVirtualBackgroundProcessors.xcframework.zip",
+            checksum: "24d9878870e6d0e5790c6fac9e092892f9d8bc4f28590d3f10469f12f732e858"
         ),
         .binaryTarget(
             name: "TwilioVirtualBackgroundProcessors-static",
-            url: "https://github.com/twilio/twilio-virtual-background-processors-ios/releases/download/99.99.99/TwilioVirtualBackgroundProcessors-static.xcframework.zip",
-            checksum: "eeb6a2abfe86cf7c8139f5b0bc7c355ed80939eb8949772cdbcd785b6b254d4e"
+            url: "https://github.com/twilio/twilio-virtual-background-processors-ios/releases/download/5.11.2/TwilioVirtualBackgroundProcessors-static.xcframework.zip",
+            checksum: "44847d589935679a32bbc0e22e9adf96c35ac810c75a6ca5e88901b361f471e1"
         )
     ]
 )
